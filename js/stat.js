725163dddb684883ec1fbcd880bde9cf7e745f5d
N.Bogdanov <nikolaj.bogdanov@24com.lv>
2019-06-14 01:06:24 +0300

    Уменьшил функцию renderStatistics, перенес вывод текста и отрисовку шкалы в отдельные функции

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,7 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var INDENT = 10;
-var MESSAGE_X = CLOUD_X + 30;
+var MESSAGE_X = CLOUD_X + 50;
 var MESSAGE_Y = CLOUD_Y + 20;
 var STRING_HEIGHT = INDENT + 10;
 var BAR = 40;
@@ -40,28 +40,32 @@ var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 };
 
-var barColor = function (name) {
+var getBarColor = function (name) {
   return (name === PLAYER_NAME) ? PLAYER_BAR_COLOR : 'hsl(240, ' + getRandomInt(0, 100) + '%, 50%)';
 };
 
+var writeText = function (ctx, text, x, y, textColor) {
+  ctx.fillStyle = textColor;
+  ctx.fillText(text, x, y);
+};
+
+var drawBar = function (ctx, names, x, y, barWidth, barHeight) {
+  ctx.fillStyle = getBarColor(names);
+  ctx.fillRect(x, y, barWidth, barHeight);
+};
+
 window.renderStatistics = function (ctx, names, times) {
+  var maxTime = getMaxElement(times);
+
   markCloud(ctx, CLOUD_X + INDENT, CLOUD_Y + INDENT, 'rgba(0, 0, 0, 0.7)', BLACK_COLOR);
   markCloud(ctx, CLOUD_X, CLOUD_Y, WHITE_COLOR, BLACK_COLOR);
-
-  ctx.fillStyle = BLACK_COLOR;
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', MESSAGE_X, MESSAGE_Y);
-  ctx.fillText('Список результатов:', MESSAGE_X, MESSAGE_Y + STRING_HEIGHT);
-
-  var maxTime = getMaxElement(times);
-  ctx.fillStyle = BLACK_COLOR;
+  writeText(ctx, 'Ура вы победили!', MESSAGE_X, MESSAGE_Y, BLACK_COLOR);
+  writeText(ctx, 'Список результатов:', MESSAGE_X, MESSAGE_Y + STRING_HEIGHT, BLACK_COLOR);
 
   for (var i = 0; i < names.length; i++) {
-
-    ctx.fillText(Math.round(times[i]), BAR_X + (BAR + BAR_INDENT) * i, BAR_Y - INDENT - (BAR_MAX_HEIGHT / maxTime) * times[i]);
-    ctx.fillStyle = barColor(names[i]);
-    ctx.fillRect(BAR_X + (BAR + BAR_INDENT) * i, BAR_Y, BAR, -+(BAR_MAX_HEIGHT / maxTime) * times[i]);
-    ctx.fillStyle = BLACK_COLOR;
-    ctx.fillText(names[i], BAR_X + (BAR + BAR_INDENT) * i, BAR_Y + STRING_HEIGHT);
+    writeText(ctx, Math.round(times[i]), BAR_X + (BAR + BAR_INDENT) * i, BAR_Y - INDENT - (BAR_MAX_HEIGHT / maxTime) * times[i], BLACK_COLOR);
+    drawBar(ctx, names[i], BAR_X + (BAR + BAR_INDENT) * i, BAR_Y, BAR, -+(BAR_MAX_HEIGHT / maxTime) * times[i]);
+    writeText(ctx, names[i], BAR_X + (BAR + BAR_INDENT) * i, BAR_Y + STRING_HEIGHT, BLACK_COLOR);
   }
 };
